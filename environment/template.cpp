91abb2490d90eee853c90593531070b304df0fce
Filip Hlasek <filip@bluevisionlabs.com>
2018-01-08 10:52:48 +0000

    Update template year.

--- environment/template.cpp
+++ environment/template.cpp
@@ -1,24 +1,23 @@
-/* Written by Filip Hlasek 2017 */
+/* Written by Filip Hlasek 2018 */
+#include <algorithm>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
 #include <cstring>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <string>
-#include <algorithm>
 #include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 
-#define FOR(i, a, b)   for (int i = (a); i <= (b); i++)
-#define FORD(i, a, b)  for (int i = (a); i >= (b); i--)
-#define REP(i, b)      for (int i =  0 ; i <  (b); i++)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); i--)
+#define REP(i, b) for (int i = 0; i < (b); i++)
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-   
-  return 0;
+int main(int argc, char* argv[]) {
+    return 0;
 }
